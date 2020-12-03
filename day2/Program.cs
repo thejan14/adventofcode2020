83eb7abb4709b8d5e253c9ba55f734769841378c
thejan14 <jan.oberpichler@googlemail.com>
2020-12-03 18:11:14 +0100

    Solution for day 2 part 2

--- day2/Program.cs
+++ day2/Program.cs
@@ -13,20 +13,26 @@
         {
             var policyAndPasswordList = File.ReadAllLines("input.txt");
 
-            var numberOfValidPasswords = 0;
+            var numberOfValidPasswordsOld = 0;
+            var numberOfValidPasswordsNew = 0;
             foreach (var policyAndPassword in policyAndPasswordList)
             {
                 var match = PolicyAndPasswordRegex.Match(policyAndPassword);
                 if (match.Success
                     && match.Groups.Count == 5
-                    && int.TryParse(match.Groups[1].Value, out var minOccurences)
-                    && int.TryParse(match.Groups[2].Value, out var maxOccurences))
+                    && int.TryParse(match.Groups[1].Value, out var leftHandPolicy)
+                    && int.TryParse(match.Groups[2].Value, out var rightHandPolicy))
                 {
                     var enforcedCharacer = Convert.ToChar(match.Groups[3].Value);
                     var password = match.Groups[4].Value;
-                    if (PasswordValid(password, enforcedCharacer, minOccurences, maxOccurences))
+                    if (PasswordValidOld(password, enforcedCharacer, leftHandPolicy, rightHandPolicy))
                     {
-                        numberOfValidPasswords += 1;
+                        numberOfValidPasswordsOld += 1;
+                    }
+
+                    if (PasswordValidNew(password, enforcedCharacer, leftHandPolicy, rightHandPolicy))
+                    {
+                        numberOfValidPasswordsNew += 1;
                     }
                 }
                 else
@@ -36,13 +42,20 @@
                 }
             }
 
-            Console.WriteLine($"Number of invalid passwords: {numberOfValidPasswords}");
+            Console.WriteLine($"(1) Number of valid passwords: {numberOfValidPasswordsOld}");
+            Console.WriteLine($"(2) Number of valid passwords: {numberOfValidPasswordsNew}");
         }
-
-        public static bool PasswordValid(string password, char enforcedCharacer, int minOccurences, int maxOccurences)
+        public static bool PasswordValidOld(string password, char enforcedCharacer, int minOccurences, int maxOccurences)
         {
             var occurences = password.Count(c => c == enforcedCharacer);
             return occurences >= minOccurences && occurences <= maxOccurences;
+        }
+
+        public static bool PasswordValidNew(string password, char enforcedCharacer, int firstPos, int secondPos)
+        {
+            var firstPosMatch = password[firstPos - 1] == enforcedCharacer;
+            var secondPosMatch = password[secondPos - 1] == enforcedCharacer;
+            return (firstPosMatch && !secondPosMatch) || (!firstPosMatch && secondPosMatch);
         }
     }
 }
