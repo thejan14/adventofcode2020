bae4ff53c75a33cfe129945a03903451a3a1fd4b
thejan14 <jan.oberpichler@googlemail.com>
2020-12-03 18:50:36 +0100

    Solution for day 3 part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
 
     public class Program
     {
@@ -10,20 +11,43 @@
         public static void Main()
         {
             var patternList = File.ReadAllLines("input.txt");
-            
+            var slopes = new (int traverseX, int traverseY)[] {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2),
+            };
+
+            var slopeTreeEncounters = slopes.Select(s => GetTreeEncounters(patternList, s.traverseX, s.traverseY)).ToArray();
+
+            Console.WriteLine($"(1) Number of tree encounters: {slopeTreeEncounters[1]}");
+
+            var productOfSlopeEncounters = 1L;
+            foreach (var treeEncounters in slopeTreeEncounters)
+            {
+                productOfSlopeEncounters *= treeEncounters;
+            }
+
+            Console.WriteLine($"(2) Prodcut of all slopes tree encounters: {productOfSlopeEncounters}");
+
+        }
+
+        public static int GetTreeEncounters(string[] patternList, int traveseX, int traverseY)
+        {
             var treeEncounters = 0;
             var posX = 0;
-            for (var posY = 0; posY < patternList.Length; posY += 1)
+            for (var posY = 0; posY < patternList.Length; posY += traverseY)
             {
                 if (CheckForTree(patternList[posY], posX))
                 {
                     treeEncounters += 1;
                 }
-                
-                posX += 3;
+
+                posX += traveseX;
             }
 
-            Console.WriteLine($"Number of tree encounters: {treeEncounters}");
+            return treeEncounters;
         }
 
         public static bool CheckForTree(string pattern, int posX)
