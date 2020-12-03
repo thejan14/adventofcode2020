0aa6746ef679eaf3d3fe14c36bc9fe0cc548e465
thejan14 <jan.oberpichler@googlemail.com>
2020-12-03 17:21:07 +0100

    Solution for day 1 part 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -6,27 +6,61 @@
 
     public class Program
     {
-        private const int targetSum = 2020;
-
-        static void Main()
+        public static void Main()
         {
             var input = File.ReadAllLines("input.txt");
             var expenses = input.Select(i => int.Parse(i)).ToArray();
 
+            _ = TryGetTwoAddends(expenses, 2020, out var leftHandAddend, out var rightHandAddend);
+            Console.WriteLine($"(1) {leftHandAddend} * {rightHandAddend} = {leftHandAddend * rightHandAddend}");
+
+            _ = TryGetThreeAddends(expenses, 2020, out var firstAddend, out var secondAddend, out var thirdAddend);
+            Console.WriteLine($"(2) {firstAddend} * {secondAddend} * {thirdAddend} = {firstAddend * secondAddend * thirdAddend}");
+        }
+
+        public static bool TryGetThreeAddends(int[] numbers, int targetSum, out int firstAddend, out int secondAddend, out int thirdAddend)
+        {
+            firstAddend = 0;
+            secondAddend = 0;
+            thirdAddend = 0;
+
             var i = 0;
-            var found = false;
-            while (!found && i < expenses.Length)
+            while (i < numbers.Length)
             {
-                var difference = targetSum - expenses[i];
-                var foundResult = expenses.Skip(i).FirstOrDefault(e => e == difference);
-                if (foundResult != 0)
+                var leftoverSum = targetSum - numbers[i];
+                if (TryGetTwoAddends(numbers.Where(n => n < leftoverSum).ToArray(), leftoverSum, out secondAddend, out thirdAddend))
                 {
-                    Console.WriteLine($"{expenses[i]} * {foundResult} = {expenses[i] * foundResult}");
-                    found = true;
+                    firstAddend = numbers[i];
+                    return true;
                 }
 
                 i += 1;
             }
+
+            return false;
+        }
+
+        public static bool TryGetTwoAddends(int[] numbers, int targetSum, out int leftHandAddend, out int rightHandAddend)
+        {
+            leftHandAddend = 0;
+            rightHandAddend = 0;
+
+            var i = 0;
+            while (i < numbers.Length)
+            {
+                var difference = targetSum - numbers[i];
+                var foundResult = numbers.Skip(i).FirstOrDefault(e => e == difference);
+                if (foundResult != 0)
+                {
+                    leftHandAddend = numbers[i];
+                    rightHandAddend = foundResult;
+                    return true;
+                }
+
+                i += 1;
+            }
+
+            return false;
         }
     }
 }
