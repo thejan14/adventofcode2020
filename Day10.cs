6e1dcf0bb7e2298ec778a8df1176ef683afb36d6
thejan14 <jan.oberpichler@googlemail.com>
2020-12-12 10:28:30 +0100

    remove unecessary usings

--- Day10.cs
+++ Day10.cs
@@ -4,8 +4,6 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Text.Json;
-    using System.Threading.Tasks;
 
     public static class Day10
     {
