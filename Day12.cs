f8afdc57cc1c3dd964d9413ccbd455727c210be2
thejan14 <jan.oberpichler@googlemail.com>
2020-12-12 19:15:02 +0100

    Solution for day 12 part 2

--- Day12.cs
+++ Day12.cs
@@ -19,38 +19,76 @@
         {
             var data = File.ReadAllLines("Day12.data");
             var instructions = data.Select(d => (d[0], int.Parse(d.Substring(1))));
-            Console.WriteLine($"Manhattan distance: {CalculateDistance(instructions, Direction.E)}");
+            Console.WriteLine($"(1) Manhattan distance (guessed instruction interpretation): {CalculateDistance(instructions, Direction.E)}");
+            Console.WriteLine($"(2) Manhattan distance (after reading the manual): {CalculateActualDistance(instructions)}");
         }
 
-        private static int CalculateDistance(IEnumerable<(char action, int value)> instructions, Direction startDirection)
+        private static int CalculateActualDistance(IEnumerable<(char action, int value)> instructions)
         {
-            var direction = startDirection;
             var horizontal = 0;
             var vertical = 0;
+
+            // relative to the ship
+            var waypointX = 10;
+            var waypointY = 1;
 
             foreach (var instruction in instructions)
             {
                 switch (instruction.action)
                 {
-                    case 'N': vertical += instruction.value; break;
-                    case 'E': horizontal += instruction.value; break;
-                    case 'S': vertical -= instruction.value; break;
-                    case 'W': horizontal -= instruction.value; break;
-                    case 'L': direction = ChangeDirectionLeft(direction, instruction.value); break;
-                    case 'R': direction = ChangeDirectionRight(direction, instruction.value); break;
+                    case 'N': waypointY += instruction.value; break;
+                    case 'E': waypointX += instruction.value; break;
+                    case 'S': waypointY -= instruction.value; break;
+                    case 'W': waypointX -= instruction.value; break;
+                    case 'L': RotateWaypoint(ref waypointX, ref waypointY, instruction.value, false); break;
+                    case 'R': RotateWaypoint(ref waypointX, ref waypointY, instruction.value, true); break;
                     case 'F':
-                        switch (direction)
-                        {
-                            case Direction.N: vertical += instruction.value; break;
-                            case Direction.E: horizontal += instruction.value; break;
-                            case Direction.S: vertical -= instruction.value; break;
-                            case Direction.W: horizontal -= instruction.value; break;
-                        }
+                        vertical += waypointX * instruction.value;
+                        horizontal += waypointY * instruction.value;
                         break;
                 }
             }
 
             return Math.Abs(horizontal) + Math.Abs(vertical);
+        }
+
+        private static void RotateWaypoint(ref int x, ref int y, int degree, bool clockWise)
+        {
+            int turns = (degree / 90) % 360;
+            for (var i = 0; i < turns; i++)
+            {
+                var tmp = x;
+                x = clockWise ? y : y * -1;
+                y = clockWise ? tmp * -1 : tmp;
+            }
+        }
+
+        private static int CalculateDistance(IEnumerable<(char action, int value)> instructions, Direction startDirection)
+        {
+            var direction = startDirection;
+            var coordinates = new Dictionary<Direction, int>()
+            {
+                { Direction.N, 0 },
+                { Direction.E, 0 },
+                { Direction.S, 0 },
+                { Direction.W, 0 },
+            };
+
+            foreach (var instruction in instructions)
+            {
+                switch (instruction.action)
+                {
+                    case 'N': coordinates[Direction.N] += instruction.value; break;
+                    case 'E': coordinates[Direction.E] += instruction.value; break;
+                    case 'S': coordinates[Direction.S] += instruction.value; break;
+                    case 'W': coordinates[Direction.W] += instruction.value; break;
+                    case 'L': direction = ChangeDirectionLeft(direction, instruction.value); break;
+                    case 'R': direction = ChangeDirectionRight(direction, instruction.value); break;
+                    case 'F': coordinates[direction] += instruction.value; break;
+                }
+            }
+
+            return Math.Abs(coordinates[Direction.N] - coordinates[Direction.S]) + Math.Abs(coordinates[Direction.E] - coordinates[Direction.W]);
         }
 
         private static Direction ChangeDirectionLeft(Direction currentDirection, int degree)
