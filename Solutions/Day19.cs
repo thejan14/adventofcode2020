7ed0977df92548b0627e6ed011f104bb4848bfd5
thejan14 <jan.oberpichler@googlemail.com>
2020-12-20 14:46:04 +0100

    solution idea for part 2

--- Solutions/Day19.cs
+++ Solutions/Day19.cs
@@ -13,11 +13,21 @@
             var data = File.ReadAllText(dataPath).Split("\n\n");
             var rulesData = data[0];
             var messages = data[1].Split('\n');
-            var ruleRegex = ParseRulesData(rulesData);
-            Console.WriteLine($"Number of valid messages: {messages.Count(m => ruleRegex.IsMatch(m))}");
+            var ruleRegex = ParseRulesData(rulesData, false);
+            Console.WriteLine($"(1) Number of valid messages: {messages.Count(m => ruleRegex.IsMatch(m))}");
+
+            var updatedRuleRegex = ParseRulesData(rulesData, true);
+            var numberOfMatches = messages.Count(m =>
+            {
+                var match = updatedRuleRegex.Match(m);
+                Console.WriteLine(match.Groups["x"].Captures.Count);
+                return match.Success;
+            });
+
+            Console.WriteLine(numberOfMatches);
         }
 
-        private static Regex ParseRulesData(string rulesData)
+        private static Regex ParseRulesData(string rulesData, bool updatedRules)
         {
             var rulesDict = new Dictionary<int, string>();
             foreach (var rule in rulesData.Split('\n').Select(s => s.Split(':')))
@@ -25,11 +35,25 @@
                 rulesDict.Add(int.Parse(rule[0]), rule[1].Trim());
             }
 
-            return new Regex($@"^{ParseRule(rulesDict, 0)}$");
+            return new Regex($@"^{ParseRule(rulesDict, 0, updatedRules)}$");
         }
 
-        private static string ParseRule(Dictionary<int, string> rulesDict, int ruleNumber)
+        private static string ParseRule(Dictionary<int, string> rulesDict, int ruleNumber, bool updatedRules)
         {
+            if (updatedRules && ruleNumber == 8)
+            {
+                return $"({ParseRule(rulesDict, 42, updatedRules)})+";
+            }
+
+            if (updatedRules && ruleNumber == 11)
+            {
+                // use balancing groups to match 42 and 31 equally often (lhs and rhs respectively)
+                // see https://docs.microsoft.com/en-us/dotnet/standard/base-types/grouping-constructs-in-regular-expressions?redirectedfrom=MSDN#balancing_group_definition
+                var rule42 = ParseRule(rulesDict, 42, updatedRules);
+                var rule31 = ParseRule(rulesDict, 31, updatedRules);
+                return $"(((?'Open'{rule42})+(?'Close-Open'{rule31})+)+(?(Open)(?!)))";
+            }
+
             var rule = rulesDict.GetValueOrDefault(ruleNumber);
             if (rule.StartsWith('"'))
             {
@@ -39,7 +63,7 @@
             {
                 var subRule = string.Join("", rule
                     .Split(' ')
-                    .Select(s => int.TryParse(s, out var num) ? ParseRule(rulesDict, num) : s));
+                    .Select(s => int.TryParse(s, out var num) ? ParseRule(rulesDict, num, updatedRules) : s));
                 return $"({subRule})";
             }
         }
