a38ae2603bdfb78da6f4827fbfaf7bc92dcd16dc
thejan14 <jan.oberpichler@googlemail.com>
2020-12-20 13:05:12 +0100

    Solution for day 19 part 1

---
+++ Solutions/Day19.cs
@@ -0,0 +1,47 @@
+﻿namespace Solution
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Day19 : Solution
+    {
+        public override void Solve(string dataPath)
+        {
+            var data = File.ReadAllText(dataPath).Split("\n\n");
+            var rulesData = data[0];
+            var messages = data[1].Split('\n');
+            var ruleRegex = ParseRulesData(rulesData);
+            Console.WriteLine($"Number of valid messages: {messages.Count(m => ruleRegex.IsMatch(m))}");
+        }
+
+        private static Regex ParseRulesData(string rulesData)
+        {
+            var rulesDict = new Dictionary<int, string>();
+            foreach (var rule in rulesData.Split('\n').Select(s => s.Split(':')))
+            {
+                rulesDict.Add(int.Parse(rule[0]), rule[1].Trim());
+            }
+
+            return new Regex($@"^{ParseRule(rulesDict, 0)}$");
+        }
+
+        private static string ParseRule(Dictionary<int, string> rulesDict, int ruleNumber)
+        {
+            var rule = rulesDict.GetValueOrDefault(ruleNumber);
+            if (rule.StartsWith('"'))
+            {
+                return rule.Substring(1, 1);
+            }
+            else
+            {
+                var subRule = string.Join("", rule
+                    .Split(' ')
+                    .Select(s => int.TryParse(s, out var num) ? ParseRule(rulesDict, num) : s));
+                return $"({subRule})";
+            }
+        }
+    }
+}
