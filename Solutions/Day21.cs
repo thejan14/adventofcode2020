38b12a0f8fe7f8512756008710407e9f9ff37513
Jan Oberpicher <oberpichler.j@eplan.de>
2020-12-22 14:33:36 +0100

    Solution for day 21 part 2

--- Solutions/Day21.cs
+++ Solutions/Day21.cs
@@ -8,36 +8,53 @@
 
     public class Day21 : Solution
     {
-        private static readonly Regex ingrediendsRegex = new Regex(@"((?:\w+ {0,1})+) \(contains ((?:\w+(?:, ){0,1})+)\)");
+        private static readonly Regex ingredientsRegex = new Regex(@"((?:\w+ {0,1})+) \(contains ((?:\w+(?:, ){0,1})+)\)");
 
         public override void Solve(string dataPath)
         {
-            var ingrediendsList = File.ReadAllLines(dataPath);
-            Console.WriteLine($"Number of inert ingredience occurences: {GetNumberOfInertIngrediends(ingrediendsList)}");
+            var ingredientsList = File.ReadAllLines(dataPath);
+            Console.WriteLine($"(1) Number of inert ingredients occurences: {GetNumberOfInertIngredients(ingredientsList, out var dangerousIngredients)}");
+            Console.WriteLine($"(2) Canonical dangerous ingredient list: {string.Join(',', dangerousIngredients)}");
         }
 
-        private static int GetNumberOfInertIngrediends(string[] ingrediendsList)
+        private static int GetNumberOfInertIngredients(string[] ingredientsList, out List<string> dangerousIngredients)
         {
-            var parsedList = ingrediendsList.Select(s =>
+            var parsedList = ingredientsList.Select(s =>
             {
-                var match = ingrediendsRegex.Match(s);
-                return (ingrediends: match.Groups[1].Value.Split(' ').ToHashSet(), allergenes: match.Groups[2].Value.Split(", "));
+                var match = ingredientsRegex.Match(s);
+                return (ingredients: match.Groups[1].Value.Split(' ').ToHashSet(), allergenes: match.Groups[2].Value.Split(", "));
             });
 
-            var ingrediends = parsedList.SelectMany(e => e.ingrediends);
+            var ingredients = parsedList.SelectMany(e => e.ingredients);
             var allergenesDict = new Dictionary<string, HashSet<string>>();
-            parsedList.SelectMany(e => e.allergenes).Distinct().ToList().ForEach(a => allergenesDict.Add(a, ingrediends.ToHashSet()));
+            parsedList.SelectMany(e => e.allergenes).Distinct().ToList().ForEach(a => allergenesDict.Add(a, ingredients.ToHashSet()));
 
             foreach (var entry in parsedList)
             {
                 foreach (var allergene in entry.allergenes)
                 {
-                    allergenesDict[allergene].IntersectWith(entry.ingrediends);
+                    allergenesDict[allergene].IntersectWith(entry.ingredients);
                 }
             }
 
-            var allergeneIngrediends = allergenesDict.Values.Aggregate(new HashSet<string>(), (acc, ingrediends) => acc.Union(ingrediends).ToHashSet()).ToList();
-            return parsedList.SelectMany(e => e.ingrediends).Count(i => !allergeneIngrediends.Contains(i));
+            var allergeneToIngredientMap = new List<(string allergene, string ingredient)>();
+            var nextUnambigousEntry = allergenesDict.FirstOrDefault(kv => kv.Value.Count == 1);
+            while (nextUnambigousEntry.Value != null)
+            {
+                var ingredientMatch = nextUnambigousEntry.Value.First();
+                allergeneToIngredientMap.Add((nextUnambigousEntry.Key, ingredientMatch));
+                foreach (var entry in allergenesDict)
+                {
+                    entry.Value.Remove(ingredientMatch);
+                }
+
+                nextUnambigousEntry = allergenesDict.FirstOrDefault(kv => kv.Value.Count == 1);
+            }
+
+            dangerousIngredients = allergeneToIngredientMap.OrderBy(map => map.allergene).Select(map => map.ingredient).ToList();
+
+            var allergeneIngredients = allergenesDict.Values.Aggregate(new HashSet<string>(), (acc, ingredients) => acc.Union(ingredients).ToHashSet()).ToList();
+            return parsedList.SelectMany(e => e.ingredients).Count(i => !allergeneIngredients.Contains(i));
         }
     }
 }
